7ee4e3900d47725b34484915e96eb945e35bcdfc
Dave Syer <dsyer@pivotal.io>
2017-11-16 12:56:59 +0000

    Test loading from a jar

--- src/main/java/com/example/bench/ProcessLauncherState.java
+++ src/main/java/com/example/bench/ProcessLauncherState.java
@@ -66,9 +66,10 @@ public class ProcessLauncherState {
 		}
 		this.length = this.args.size();
 		this.args.add("--server.port=0");
-		this.args.add("--function.uri=file:"
-				+ new File(projectHome + "/target/test-classes").getAbsolutePath()
-				+ "?io.sk8s.invoker.java.function.Doubler");
+		this.args.add("--function.uri=file:" + new File(System.getProperty("user.home")
+				+ "/.m2/repository/io/spring/sample/function-sample-pof/1.0.0.BUILD-SNAPSHOT/function-sample-pof-1.0.0.BUILD-SNAPSHOT.jar")
+						.getAbsolutePath()
+				+ "?functions.Greeter");
 		this.args.addAll(Arrays.asList(args));
 		this.home = new File(dir);
 	}
